61bb39545c4783aec53b0507c91a7b16196834d5
Archiki Prasad <archikiprasad@gmail.com>
2018-10-21 02:15:12 +0530

    Modified Dataloader, corrected paths, loaded filenames..

--- code/data_loader.py
+++ code/data_loader.py
@@ -3,54 +3,57 @@ from torchvision import transforms
 from skimage import io, transform
 import os
 import numpy as np
-
+import re
 
 class SourceSepTrain(Dataset):
-    def __init__(self, path='data/train/'):
+    def __init__(self, path='../Processed/Mixtures'): 
+    # assuming this to be the directory containing all the magnitude spectrum 
+    #for all songs and all segments used in training
         self.path = path
         self.list = os.listdir(self.path)
 
     def __getitem__(self, index):
-        mixture_path = 'data/train/Mixture/'
-        base_path = 'data/train/Base/'
-        vocals_path = 'data/train/Vocals/'
-        drums_path = 'data/train/Drums/'
-        others_path = 'data/train/Others/'
+        mixture_path = '../Processed/Mixtures/'
+        bass_path = '../Processed/Bass/'
+        vocals_path = '../Processed/Vocals/'
+        drums_path = '../Processed/Drums/'
+        others_path = '../Processed/Others/'
+        mixture = torch.load(mixture_path+self.list[index])
+        #phase = torch.load(mixture_path+self.list[index]+'_p')
+        bass = torch.load(bass_path+self.list[index])
+        vocals = torch.load(vocals_path+self.list[index])
+        drums = torch.load(drums_path+self.list[index])
+        others = torch.load(others_path+self.list[index])
 
-        mixture = torch.load(mixture_path+self.list[index]+'_m')
-        phase = torch.load(mixture_path+self.list[index]+'_p')
-        base = torch.load(base_path+self.list[index]+'_m')
-        vocals = torch.load(vocals_path+self.list[index]+'_m')
-        drums = torch.load(drums_path+self.list[index]+'_m')
-        others = torch.load(others_path+self.list[index]+'_m')
-
-        return (mixture, phase, base, vocals, drums, others)
+        return (mixture,bass, vocals, drums, others)
 
     def __len__(self):
-        return len(self.list)/6 # of how much data you have
+        return len(self.list) # length of how much data you have
 
 
 class SourceSepTest(Dataset):
-    def __init__(self, path='data/val/'):
+    def __init__(self, path='../Val/Mixtures'):
+        # assuming this to be the directory containing all the magnitude spectrum 
+        #for all songs and all segments used in training
         self.path = path
         self.list = os.listdir(self.path)
 
     def __getitem__(self, index):
         # stuff
-        mixture_path = 'data/val/Mixture/'
-        base_path = 'data/val/Base/'
-        vocal_path = 'data/val/Vocals/'
-        drums_path = 'data/val/Drums/'
-        others_path = 'data/val/Others/'
+        mixture_path = '../Val/Mixtures/'
+        bass_path = '../Val/Bass/'
+        vocal_path = '../Val/Vocals/'
+        drums_path = '../Val/Drums/'
+        others_path = '../Val/Others/'
 
-        mixture = torch.load(mixture_path+self.list[index]+'_m')
-        phase = torch.load(mixture_path+self.list[index]+'_p')
-        base = torch.load(base_path+self.list[index]+'_m')
-        vocals = torch.load(vocals_path+self.list[index]+'_m')
-        drums = torch.load(drums_path+self.list[index]+'_m')
-        others = torch.load(others_path+self.list[index]+'_m')
+        mixture = torch.load(mixture_path+self.list[index])
+        #phase = torch.load(mixture_path+self.list[index]+'_p')
+        bass = torch.load(bass_path+self.list[index])
+        vocals = torch.load(vocals_path+self.list[index])
+        drums = torch.load(drums_path+self.list[index])
+        others = torch.load(others_path+self.list[index])
 
-        return (mixture, phase, base, vocals, drums, others)
+        return (mixture, bass, vocals, drums, others)
 
     def __len__(self):
-        return len(self.list)/6
+        return len(self.list)

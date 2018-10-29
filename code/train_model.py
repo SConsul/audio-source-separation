bff262d18734668281b956ad8ca26165e3fb35bf
Archiki Prasad <archikiprasad@gmail.com>
2018-10-29 19:35:57 +0530

    adding others in training

--- code/train_model.py
+++ code/train_model.py
@@ -50,7 +50,7 @@ class MixedSquaredError(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(MixedSquaredError, self).__init__()
 
-    def forward(self, pred_bass,pred_vocals,pred_drums,pred_others, gt_bass,gt_vocals,gt_drums):
+    def forward(self, pred_bass,pred_vocals,pred_drums,pred_others, gt_bass,gt_vocals,gt_drums,gt_others):
         L_sq = np.sum(np.square(pred_bass-gt_bass)) + np.sum(np.square(pred_vocals-gt_vocals)) + np.sum(np.square(pred_drums-gt_drums))
         L_other = np.sum(np.square(pred_bass-pred_others)) + np.sum(np.square(pred_drums-pred_others)) + np.sum(np.square(pred_vocals-pred_others))
         L_othervocals = np.sum(np.square(pred_vocals - pred_others))
@@ -70,12 +70,12 @@ class TimeFreqMasking(nn.Module):
 
         return bass,vocals,drums,others
 
-train_set = SourceSepTrain(transforms = None)
+train_set = SourceSepTrain()
 
 def train():
     cuda = torch.cuda.is_available()
     net = SepConvNet(t1,f1,t2,f2,N1,N2,inp_size,NN)
-    criterion = nn.MixedSquaredError()
+    criterion = MixedSquaredError()
     if cuda:
         net = net.cuda()
         criterion = criterion.cuda()
@@ -84,29 +84,30 @@ def train():
     print("preparing training data ...")
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     print("done ...")
-    val_set = SourceSepVal(transforms = None)
+    val_set = SourceSepVal()
     val_loader = DataLoader(val_set, batch_size=batch_size,shuffle=False)
 
     for epoch in range(num_epochs):
         train_loss = Average()
 
         net.train()
-        for i, (input, gt_bass,gt_drums,gt_vocals) in tqdm(enumerate(train_loader)):
-            input = Variable(input)
+        for i, (inp, gt_bass,gt_drums,gt_vocals,gt_others) in tqdm(enumerate(train_loader)):
+            inp = Variable(inp)
             gt_bass = Variable(gt_bass)
             gt_vocals = Variable(gt_vocals)
             gt_drums = Variable(gt_drums)
+            gt_others= Variable(gt_others)
             if cuda:
-                input = input.cuda()
+                inp = inp.cuda()
                 gt_bass = gt_bass.cuda()
                 gt_vocals = gt_vocals.cuda()
                 gt_drums = gt_drums.cuda()
-
+                gt_others= gt_others.cuda()
             optimizer.zero_grad()
-            o_bass, o_vocals, o_drums, o_others = net(input)
+            o_bass, o_vocals, o_drums, o_others = net(inp)
             pred_bass,pred_vocals,pred_drums,pred_others = TimeFreqMasking(o_bass, o_vocals, o_drums, o_others)
 
-            loss = criterion(pred_bass,pred_vocals,pred_drums,pred_others, gt_bass,gt_vocals,gt_drums)
+            loss = criterion(pred_bass,pred_vocals,pred_drums,pred_others, gt_bass,gt_vocals,gt_drums,gt_others)
             writer.add_scalar('Train Loss',loss,epoch)
             loss.backward()
             optimizer.step()

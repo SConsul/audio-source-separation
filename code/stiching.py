corrected test_model for normalization

code/stiching.py
@@ -42,7 +42,7 @@ for test_songs in (test_songs_list):
         os.makedirs(vocals_path)
     sound_output_path = os.path.join(vocals_path,test_songs+'.wav')
     for segment in (vocals_list) :
-        seg, sr = librosa.load(segment)
+        seg, sr = librosa.load(segment, sr=44100)
         print(sr)
         assert sr==44100
         combined_vocals= np.append(combined_vocals,seg)
@@ -58,7 +58,8 @@ for test_songs in (test_songs_list):
         os.makedirs(bass_path)
     sound_output_path = os.path.join(bass_path,test_songs+'.wav')
     for segment in (bass_list) :
-        seg, sr = librosa.load(segment)
+        seg, sr = librosa.load(segment,sr=44100)
+        assert sr==44100
         combined_bass= np.append(combined_bass,seg)
     librosa.output.write_wav(sound_output_path,combined_bass,sr)
 
@@ -72,7 +73,7 @@ for test_songs in (test_songs_list):
         os.makedirs(drums_path)
     sound_output_path = os.path.join(drums_path,test_songs+'.wav')
     for segment in (drums_list) :
-        seg, sr = librosa.load(segment)
+        seg, sr = librosa.load(segment,sr=44100)
         combined_drums= np.append(combined_drums,seg)
     librosa.output.write_wav(sound_output_path,combined_drums,sr)
 
@@ -85,6 +86,6 @@ for test_songs in (test_songs_list):
         os.makedirs(others_path)
     sound_output_path = os.path.join(others_path,test_songs+'.wav')
     for segment in (others_list) :
-        seg, sr = librosa.load(segment)
+        seg, sr = librosa.load(segment,sr=44100)
         combined_others= np.append(combined_others,seg)
     librosa.output.write_wav(sound_output_path,combined_others,sr)

code/test_model.py
@@ -3,7 +3,7 @@ import numpy as np
 import glob
 import re
 import os
-from build_model import SepConvNet
+from build_model_original import SepConvNet
 from torch.utils.data import DataLoader
 from data_loader import SourceSepTest
 from post_processing import reconstruct
@@ -46,14 +46,18 @@ if __name__ == '__main__':
 
     net = SepConvNet(t1,f1,t2,f2,N1,N2,inp_size,NN)
     # net.load_state_dict(torch.load('Weights/Weights_200_3722932.6015625.pth')) #least score Weights so far
-    net.load_state_dict(torch.load('Weights/Weights_97_39969.63211206897.pth'))
+    net.load_state_dict(torch.load('Weights/Weights_norm_orig2.pth'))
     net.eval()
     test_set = SourceSepTest(transforms = None)
     test_loader = DataLoader(test_set, batch_size=batch_size,shuffle=False)
     for i,(test_inp,test_phase_file,file_str) in tqdm(enumerate(test_loader)):
         print('Testing, i='+str(i))
         test_phase = np.load(phase_path+test_phase_file[0])
-        bass_mag, vocals_mag, drums_mag,others_mag = net(test_inp)
+        
+        mean = torch.mean(test_inp)
+        std = torch.std(test_inp)
+        test_inp_n = (test_inp-mean)/std
+        bass_mag, vocals_mag, drums_mag,others_mag = net(test_inp_n)
         bass_mag, vocals_mag, drums_mag,others_mag = TimeFreqMasking(bass_mag, vocals_mag, drums_mag,others_mag)
         bass_mag = bass_mag*test_inp
         vocals_mag = vocals_mag*test_inp

code/train_model.py
@@ -45,11 +45,11 @@ f2=1
 N1=50
 N2=30
 NN=128
-alpha = 0.001
-beta = 0.01
-beta_vocals = 0.03
+alpha = 0.005
+beta = 0.05
+beta_vocals = 0.08
 batch_size = 30
-num_epochs = 100
+num_epochs = 50
 
 
 class MixedSquaredError(nn.Module):